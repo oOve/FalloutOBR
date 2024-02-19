28f59487b56a315862c37bc98df1414ec22d5be6
Dr. O_o <oOve@users.noreply.github.com>
2024-02-19 12:32:39 +0100

    Initial

---
+++ src/tray.js
@@ -0,0 +1,22 @@
+import OBR from "@owlbear-rodeo/sdk";
+
+function getPluginId(path) {
+    return `rodeo.owlbear.dice/${path}`;
+  }
+
+
+  OBR.onReady(() => {
+    OBR.popover.open({
+      id: getPluginId("popover"),
+      url: "/popover.html",
+      width: 0,
+      height: 0,
+      anchorOrigin: { horizontal: "RIGHT", vertical: "BOTTOM" },
+      transformOrigin: { horizontal: "RIGHT", vertical: "BOTTOM" },
+      disableClickAway: true,
+      hidePaper: true,
+      marginThreshold: 0,
+    });
+  });
+
+  
