9422b7bc4b44b8e2b21f1caeb259b49c4d9fa80f
Dr. O_o <oOve@users.noreply.github.com>
2024-02-19 13:54:49 +0100

    stumbling around in the dark

--- src/tray.js
+++ src/tray.js
@@ -1,20 +1,16 @@
-import OBR from "@owlbear-rodeo/sdk";
 
-function getPluginId(path) {
-    return `rodeo.owlbear.dice/${path}`;
-  }
+
 
 
   OBR.onReady(() => {
     OBR.popover.open({
-      id: getPluginId("popover"),
       url: "./popover.html",
       width: 0,
       height: 0,
       anchorOrigin: { horizontal: "RIGHT", vertical: "BOTTOM" },
       transformOrigin: { horizontal: "RIGHT", vertical: "BOTTOM" },
       disableClickAway: true,
-      hidePaper: true,
+      hidePaper: false,
       marginThreshold: 0,
     });
   });
